e32873747d5d0c6a06bff66735d83768cdd47f12
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-02-22 18:09:53 +0530

    fixed filter

--- js/filters.js
+++ js/filters.js
@@ -10,20 +10,39 @@ const priceSlider = () => {
     }
 }
 
-const collapseFilter = () => {
-    const filter = document.querySelector(".filter__container");
-    const filterBtn = document.querySelector(".filter__btn");
-    filterBtn.addEventListener() = () => {
-        filter.classList.add("filter__container--collapsed");
-    }
-}
+// const collapseFilter = () => {
+//     const filter = document.querySelector(".filter__container");
+//     const filterBtn = document.querySelector(".filter__btn");
+//     filterBtn.addEventListener() = () => {
+//         filter.classList.add("filter__container--collapsed");
+//     }
+// }
+
+// const openFilter = () => {
+//     const filterBtn = document.querySelector(".filter__btn");
+//     filterBtn.addEventListener("click", () => {
+//         console.log("clicked");
+//         const filter = document.querySelector(".filter__container");
+//         filter.classList.add("filter--active");
+//     })
+// }
+
+// openFilter();
 
 const clearFilters = () => {
     const clearFilterBtn = document.querySelector(".filter__clear");
+
+    // For price slider
+    const slider = document.getElementById("price-range");
+    const priceFilter = document.querySelector(".price-range-output");
+    
     clearFilterBtn.addEventListener("click", () => {
         const data = document.querySelectorAll(".filter-data");
         data[0].reset();
         data[1].reset();
+
+        //Resetting price slider ouput. 
+        priceFilter.innerHTML = `₹100 to ₹${slider.value}`;
     })
 }
 
